acbe71009de508efa59d032738bc434e45c0d27f
castiel10k <71992808+castiel10k@users.noreply.github.com>
2022-09-18 14:51:47 -0400

    ajout des augmentations et restructuration du fichier sql

--- CreationDND/MainWindow.xaml.cs
+++ CreationDND/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
 //using static dbHandler;
 //using static raceCharacter;
 
@@ -30,8 +29,7 @@ namespace CreationDND
         {
             
             InitializeComponent();
-            
-            
+
         }
     }
 }
