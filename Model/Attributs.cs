a8ab2e3a54b6b379ed46a40c304f2e97c1bcf36a
KhalilAlhelou <64221881+KhalilAlhelou@users.noreply.github.com>
2022-10-03 13:45:12 -0400

    ClassDTO et AttributsDTO sont prêts

---
+++ Model/Attributs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Attributs
+    {
+        public Attributs (string nomAttribut, string descriptionAttribut)
+        {
+
+            NomAttribut = nomAttribut;
+            DescriptionAttribut = descriptionAttribut;
+
+
+        }
+
+        public string NomAttribut
+        {
+            get;
+            private set;
+        }
+
+        public string DescriptionAttribut
+        {
+            get;
+            private set;
+        }
+
+    }
+}
