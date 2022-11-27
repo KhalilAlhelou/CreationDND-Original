dcb98869503c3a58824ff7b2be8749ae29c96fdc
KhalilAlhelou <64221881+KhalilAlhelou@users.noreply.github.com>
2022-11-26 20:20:02 -0500

    .

---
+++ CreationDND/InterfaceStats.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ViewModel;
+
+namespace CreationDND
+{
+    /// <summary>
+    /// Lógica interna para InterfaceStats.xaml
+    /// </summary>
+    public partial class InterfaceStats : Window
+    {
+        private ViewModels _viewModel;
+        public InterfaceStats()
+        {
+            InitializeComponent();
+            _viewModel = ViewModels.getInstance;
+            _viewModel.inserserStats();
+            DataContext = _viewModel;
+        }
+    }
+}
